92058d6fe6b8be58df32272ac2334f63428ac70d
”MeHighLove” <mikhailov_danya@mail.ru>
2020-11-12 17:10:58 +0300

    Time to the first review

--- test/player.cpp
+++ test/player.cpp
@@ -4,11 +4,12 @@
 TEST(player, ok) {
     Player pl;
     Object_dynamic od;
+    int distance = abs(pl.get_center().x - od.get_center().x);
     pl.take_elem(od);
     Point p;
     Speed s;
     pl.set_center(p);
     pl.set_speed(s);
-    ASSERT_EQ(p.x, od.get_center().x);
+    ASSERT_EQ(p.x + distance, od.get_center().x);
     ASSERT_EQ(s.dx, od.get_speed().dx);
 }
