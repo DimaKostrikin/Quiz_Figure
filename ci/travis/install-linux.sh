a18f1ef841791f4b0ca810c83639c7c9273e0919
Anastasia Morozova <morozovaanastasiaa2001@gmail.com>
2020-12-24 13:27:57 +0300

    gcc fixed

--- ci/travis/install-linux.sh
+++ ci/travis/install-linux.sh
@@ -3,7 +3,7 @@
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get update -qq
 
-sudo apt-get install -qq g++-5
+sudo apt-get install g++-7 -y
 sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-5 90
 
 sudo apt install valgrind
