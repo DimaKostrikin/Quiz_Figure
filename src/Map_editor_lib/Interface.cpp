543b5602ec2aa8a6e29a94325e19554da756e5d0
Anastasia Morozova <morozovaanastasiaa2001@gmail.com>
2020-12-27 00:14:56 +0300

    Models added

--- src/Map_editor_lib/Interface.cpp
+++ src/Map_editor_lib/Interface.cpp
@@ -28,8 +28,8 @@ Interface::~Interface() = default;
 Interface::Interface() {
     // glfw: инициализация и конфигурирование
     glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     // glfw: создание окна
