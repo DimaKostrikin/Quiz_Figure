8e82bad8a7ab9eb8674c49f26c64ced5f5624ad0
Dmitry Kostrikin <dima.da222@yandex.ru>
2020-12-26 05:18:23 +0300

    fans works

--- src/Features_lib/Handler_feature.cpp
+++ src/Features_lib/Handler_feature.cpp
@@ -55,19 +55,19 @@ void Handler_feature::action(std::list<Object_dynamic>::iterator &obj_d, std::li
     }
 
     if (obj_a->get_elem_type() == FAN) {  // Обработка попадания в зону вентилятора
-        float a_radius = sqrt(obj_a->get_size().y + obj_a->get_size().x);
+        float a_radius = sqrt(obj_a->get_size().y + obj_a->get_size().x) * 1.1;
 
-        float a_height = 3;
-        float acceleration_coeff = 2;
+        float a_height = 5;
+        float acceleration_coeff = 0;
 
         if (event_radius_obj < a_radius && diff_z_obj < a_height) {
             acceleration_coeff = (a_height - diff_z_obj) / a_height;
-            obj_d->get_speed().z += acceleration_coeff * elapsed_time; // TODO коэффициент скорости отладить
+            obj_d->get_speed().z += acceleration_coeff * elapsed_time * 10; // TODO коэффициент скорости отладить
         }
 
         if (event_radius_pl < a_radius && diff_z_pl < a_height) {
             acceleration_coeff = (a_height - diff_z_pl) / a_height;
-            player.get_speed().z += acceleration_coeff * elapsed_time; // TODO коэффициент скорости отладить
+            player.get_speed().z += acceleration_coeff  * elapsed_time * 10;// TODO коэффициент скорости отладить
         }
     }
 
@@ -85,21 +85,21 @@ void Handler_feature::action(std::list<Object_dynamic>::iterator &obj_d, std::li
         }
     }
 
-    if (obj_a->get_elem_type() == TELEPORT) {
-        float a_radius = 0.1;
-        float a_height = 0.1;
+    if (obj_a->get_elem_type() == TELEPORT_IN) {
+        float a_radius = 1.1;  // TODO поменять радиусы
+        float a_height = 1.1;
 
 
         if (event_radius_obj < a_radius && diff_z_obj < a_height) {
-            obj_d->get_center().x = obj_a->get_center().x;
-            obj_d->get_center().y = obj_a->get_center().y;
-            obj_d->get_center().z = obj_a->get_center().z;
+            obj_d->get_center().x = obj_a->get_linked_object()->get_center().x;
+            obj_d->get_center().y = obj_a->get_linked_object()->get_center().y;
+            obj_d->get_center().z = obj_a->get_linked_object()->get_center().z + obj_a->get_linked_object()->get_size().z;
         }
 
         if (event_radius_pl < a_radius && diff_z_pl < a_height) {
-            player.get_center().x = obj_a->get_center().x;
-            player.get_center().y = obj_a->get_center().y;
-            player.get_center().z = obj_a->get_center().z;
+            player.get_center().x = obj_a->get_linked_object()->get_center().x;
+            player.get_center().y = obj_a->get_linked_object()->get_center().y;
+            player.get_center().z = obj_a->get_linked_object()->get_center().z + obj_a->get_linked_object()->get_size().z;
         }
     }
 }
